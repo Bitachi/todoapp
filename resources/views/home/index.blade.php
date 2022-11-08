a5512e6de46a572139430dca454e91abedb89b9d
GoTakami <funkykong888@gmail.com>
2022-11-08 22:04:37 +0900

    second commit

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Todo/Index</title>
+        <link rel="stylesheet" href="{{asset('/css/style.css')}}" >
+    </head>
+    <body>
+        <header>
+            Todo App
+        </header>
+        <main>
+            <p>今日は何する？</p>
+        </main>
+        <footer>
+            Copyright: Takami 2022.
+        </footer>
+    </body>
+</html>
